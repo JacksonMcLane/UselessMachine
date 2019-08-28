1a37b5899c444c6fa56f407e9e8d58457d753b12
JacksonMcLane <jackson.mclane20@gmail.com>
2019-08-28 13:57:59 -0700

    Added Switch Listener

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.uselessmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch useless;
+    private Button selfDestruct;
+    private Button lookBusy;
+    private ProgressBar progressBar;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wireWidgets();
+        setListeners();
+        updateAndRandomize();
     }
+
+    private void updateAndRandomize() {
+
+    }
+
+    private void setListeners() {
+        selfDestruct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        lookBusy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.getVisibility();
+            }
+        });
+
+        useless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, final boolean isChecked) {
+                if(isChecked) {
+                    int time = (int) (Math.random() * 3000);
+                    new CountDownTimer(time, 10) {
+                        public void onFinish() {
+                            useless.setChecked(false);
+                        }
+
+                        @Override
+                        public void onTick(long l) {
+                            if(!useless.isChecked()) {
+                                cancel(); //cancels timer if user switches the switch off
+                            }
+                        }
+                    }.start();
+                }
+//                if(isChecked) {
+//                    Toast.makeText(MainActivity.this, "ON", Toast.LENGTH_SHORT).show();
+//                }
+//                else
+//                    Toast.makeText(MainActivity.this, "OFF", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+    private void wireWidgets() {
+        useless = findViewById(R.id.switch_main_useless);
+        selfDestruct = findViewById(R.id.button_main_self_destruct);
+        lookBusy = findViewById(R.id.button_main_look_busy);
+        progressBar = findViewById(R.id.progressBar_main_progress);
+        constraintLayout = findViewById(R.id.constraintlayout_main);
+    }
+
+
 }
--- app/src/main/java/com/example/uselessmachine/UselessMachine.java
+++
@@ -1,5 +0,0 @@
-package com.example.uselessmachine;
-
-public class UselessMachine {
-
-}
